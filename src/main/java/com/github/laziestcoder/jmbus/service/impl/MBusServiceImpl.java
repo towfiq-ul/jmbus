ad6dc4270173956000ffb698686d5eea9ecdda86
towfiq <towfiq.106@gmail.com>
2023-02-08 02:02:12 +0600

    feat: libmbus.so library problem solved

--- src/main/java/com/github/laziestcoder/jmbus/service/impl/MBusServiceImpl.java
+++ src/main/java/com/github/laziestcoder/jmbus/service/impl/MBusServiceImpl.java
@@ -8,18 +8,31 @@ import com.github.laziestcoder.jmbus.mbus.mbus_frame;
 import com.github.laziestcoder.jmbus.mbus.mbus_frame_data;
 import com.github.laziestcoder.jmbus.service.MBusService;
 import com.sun.jna.Native;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.InputStream;
+import java.net.URL;
 
 @Service
 public class MBusServiceImpl implements MBusService {
-    private final Logger LOG = Logger.getLogger(MBusService.class.getName());
-    private final String LIB_DIR = new ClassPathResource("libmbus").getPath();
+    private static final Logger LOG = LoggerFactory.getLogger(MBusService.class.getName());
+    private static final String SO_FILE_PATH_NAME = "libmbus.so";
     private final XmlMapper xmlMapper = new XmlMapper();
-    private final LibMbus libMbus = Native.load(LIB_DIR, LibMbus.class);
+    private LibMbus libMbus = null;
+
+    public MBusServiceImpl() {
+        try {
+            ClassPathResource LIB_RESOURCE = new ClassPathResource(SO_FILE_PATH_NAME, this.getClass().getClassLoader());
+            String LIB_DIR = LIB_RESOURCE.getPath();
+            LOG.info("Libmbus Path: {}", LIB_DIR);
+            libMbus = Native.load(LIB_DIR, LibMbus.class);
+        } catch (Exception e) {
+            LOG.error("Exception: {}", e.getMessage(), e);
+        }
+    }
 
     @Override
     public JsonNode decodeMessage(JsonNode dataNode, String propertyName) {
@@ -35,7 +48,7 @@ public class MBusServiceImpl implements MBusService {
     private JsonNode getDecodedValue(String hexValue) {
         JsonNode jsonResult = null;
         try {
-            byte[] buff = new byte[4096];
+            byte[] buff = new byte[5120];
             byte[] raw_buff = hexValue.getBytes();
             long buffLength = libMbus.mbus_hex2bin(buff, buff.length, raw_buff, raw_buff.length);
             mbus_frame mBusFrame = new mbus_frame();
@@ -46,7 +59,8 @@ public class MBusServiceImpl implements MBusService {
             String result = libMbus.mbus_frame_data_xml(frame_data);
             jsonResult = xmlMapper.readTree(result.getBytes());
         } catch (Exception e) {
-            LOG.log(Level.INFO, e.getMessage(), e);
+            LOG.error(e.getMessage(), e);
+            throw new RuntimeException(e.getMessage());
         }
         return jsonResult;
     }
---
+++ src/test/java/com/github/laziestcoder/jmbus/service/impl/MBusServiceTest.java
@@ -0,0 +1,25 @@
+package com.github.laziestcoder.jmbus.service.impl;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.github.laziestcoder.jmbus.service.MBusService;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Objects;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@SpringBootTest
+public class MBusServiceTest {
+    public MBusService mBusService = new MBusServiceImpl();
+    private static final String hexValue = "68 AE AE 68 28 01 72 95 08 12 11 83 14 02 04 17 00 00 00 84 00 86 3B 23 00 00 00 84 0086 3C D1 01 00 00 84 40 86 3B 00 00 00 00 84 40 86 3C 00 00 00 00 85 00 5B 2B 4B AC 41 85 00 5F 20 D7 AC 41 85 40 5B 00 00 B8 42 85 40 5F 00 00 B8 42 85 00 3B 84 00 35 3F 85 40 3B 00 00 00 00 95 00 3B 95 CF B2 43 95 40 3B 00 00 00 00 85 00 2B 00 00 00 00 8540 2B 00 00 00 00 95 00 2B D3 9F 90 46 95 40 2B 00 00 00 00 04 6D 19 0F 8A 17 84 00 7C 01 43 F3 0D 00 00 84 40 7C 01 43 9D 01 00 00 84 00 7C 01 63 01 00 00 00 84 40 7C 01 63 01 00 00 00 0F 2F 16";
+    private static final String decodedHex = "{\"SlaveInformation\":{\"Id\":\"11120895\",\"Manufacturer\":\"EDC\",\"Version\":\"2\",\"ProductName\":\"\",\"Medium\":\"Heat: Outlet\",\"AccessNumber\":\"23\",\"Status\":\"00\",\"Signature\":\"0000\"},\"DataRecord\":[{\"id\":\"0\",\"Function\":\"Instantaneous value\",\"StorageNumber\":\"0\",\"Tariff\":\"0\",\"Device\":\"0\",\"Unit\":\"Energy (kWh)\",\"Value\":\"35\"},{\"id\":\"1\",\"Function\":\"Instantaneous value\",\"StorageNumber\":\"0\",\"Tariff\":\"0\",\"Device\":\"0\",\"Unit\":\"Energy (kWh)\",\"Value\":\"465\"},{\"id\":\"2\",\"Function\":\"Instantaneous value\",\"StorageNumber\":\"0\",\"Tariff\":\"0\",\"Device\":\"1\",\"Unit\":\"Energy (kWh)\",\"Value\":\"0\"},{\"id\":\"3\",\"Function\":\"Instantaneous value\",\"StorageNumber\":\"0\",\"Tariff\":\"0\",\"Device\":\"1\",\"Unit\":\"Energy (kWh)\",\"Value\":\"0\"},{\"id\":\"4\",\"Function\":\"Instantaneous value\",\"StorageNumber\":\"0\",\"Tariff\":\"0\",\"Device\":\"0\",\"Unit\":\"Flow temperature (deg C)\",\"Value\":\"21.536703\"},{\"id\":\"5\",\"Function\":\"Instantaneous value\",\"StorageNumber\":\"0\",\"Tariff\":\"0\",\"Device\":\"0\",\"Unit\":\"Return temperature (deg C)\",\"Value\":\"21.605042\"},{\"id\":\"6\",\"Function\":\"Instantaneous value\",\"StorageNumber\":\"0\",\"Tariff\":\"0\",\"Device\":\"1\",\"Unit\":\"Flow temperature (deg C)\",\"Value\":\"92.000000\"},{\"id\":\"7\",\"Function\":\"Instantaneous value\",\"StorageNumber\":\"0\",\"Tariff\":\"0\",\"Device\":\"1\",\"Unit\":\"Return temperature (deg C)\",\"Value\":\"92.000000\"},{\"id\":\"8\",\"Function\":\"Instantaneous value\",\"StorageNumber\":\"0\",\"Tariff\":\"0\",\"Device\":\"0\",\"Unit\":\"Volume flow (m m^3/h)\",\"Value\":\"0.707039\"},{\"id\":\"9\",\"Function\":\"Instantaneous value\",\"StorageNumber\":\"0\",\"Tariff\":\"0\",\"Device\":\"1\",\"Unit\":\"Volume flow (m m^3/h)\",\"Value\":\"0.000000\"},{\"id\":\"10\",\"Function\":\"Maximum value\",\"StorageNumber\":\"0\",\"Tariff\":\"0\",\"Device\":\"0\",\"Unit\":\"Volume flow (m m^3/h)\",\"Value\":\"357.621735\"},{\"id\":\"11\",\"Function\":\"Maximum value\",\"StorageNumber\":\"0\",\"Tariff\":\"0\",\"Device\":\"1\",\"Unit\":\"Volume flow (m m^3/h)\",\"Value\":\"0.000000\"},{\"id\":\"12\",\"Function\":\"Instantaneous value\",\"StorageNumber\":\"0\",\"Tariff\":\"0\",\"Device\":\"0\",\"Unit\":\"Power (W)\",\"Value\":\"0.000000\"},{\"id\":\"13\",\"Function\":\"Instantaneous value\",\"StorageNumber\":\"0\",\"Tariff\":\"0\",\"Device\":\"1\",\"Unit\":\"Power (W)\",\"Value\":\"0.000000\"},{\"id\":\"14\",\"Function\":\"Maximum value\",\"StorageNumber\":\"0\",\"Tariff\":\"0\",\"Device\":\"0\",\"Unit\":\"Power (W)\",\"Value\":\"18511.912109\"},{\"id\":\"15\",\"Function\":\"Maximum value\",\"StorageNumber\":\"0\",\"Tariff\":\"0\",\"Device\":\"1\",\"Unit\":\"Power (W)\",\"Value\":\"0.000000\"},{\"id\":\"16\",\"Function\":\"Instantaneous value\",\"StorageNumber\":\"0\",\"Unit\":\"Time Point (time & date)\",\"Value\":\"2012-07-10T15:25:00\"},{\"id\":\"17\",\"Function\":\"Instantaneous value\",\"StorageNumber\":\"0\",\"Tariff\":\"0\",\"Device\":\"0\",\"Unit\":\"C\",\"Value\":\"3571\"},{\"id\":\"18\",\"Function\":\"Instantaneous value\",\"StorageNumber\":\"0\",\"Tariff\":\"0\",\"Device\":\"1\",\"Unit\":\"C\",\"Value\":\"413\"},{\"id\":\"19\",\"Function\":\"Instantaneous value\",\"StorageNumber\":\"0\",\"Tariff\":\"0\",\"Device\":\"0\",\"Unit\":\"c\",\"Value\":\"1\"},{\"id\":\"20\",\"Function\":\"Instantaneous value\",\"StorageNumber\":\"0\",\"Tariff\":\"0\",\"Device\":\"1\",\"Unit\":\"c\",\"Value\":\"1\"},{\"id\":\"21\",\"Function\":\"Manufacturer specific\",\"Value\":\"\"}]}";
+
+    @Test
+    public void decodeMessage() {
+        JsonNode result = mBusService.decodeMessage(hexValue);
+        assertEquals(decodedHex, result.toString());
+    }
+
+}
